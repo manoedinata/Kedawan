1529d00681618f50defced2c8bbe72088c40ef38
Hendra <manoedinata@gmail.com>
2023-05-21 19:36:27 +0700

    Adjust DB models and remove the old one

--- kedawan/db.py
+++ kedawan/db.py
@@ -4,32 +4,8 @@ from sqlalchemy.dialects.mysql import INTEGER
 
 db = SQLAlchemy()
 
-# FastLinks
-# class FastLinks(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     slug = db.Column(db.Text(10), unique=True, nullable=False)
-#     url = db.Column(db.Text(255), nullable=False)
-#     visitor = db.Column(db.Integer, default=0)
-#     created_at = db.Column(db.DateTime, nullable=False)
-#     expire = db.Column(db.DateTime, nullable=False)
-
-#     @property
-#     def serialize(self):
-#         """Return object data in easily serializable format"""
-#         """https://stackoverflow.com/a/7103486"""
-#         # TODO: Add serialize property for created_at
-#         # and expire
-#         return {
-#             "id": self.id,
-#             "slug": self.slug,
-#             "url": self.url,
-#         }
-
-#     def __repr__(self):
-#         return f'<FastLinks {self.id}>'
-
 class FastLinks(db.Model):
-    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.Text, nullable=False)
     url = db.Column(db.Text, nullable=False)
     expire = db.Column(db.DateTime, nullable=False)
@@ -53,19 +29,17 @@ class FastLinks(db.Model):
         return f'<FastLinks {self.id}>'
 
 class Visitor(db.Model):
-    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
-    fast_links_id = db.Column(INTEGER(unsigned=True), db.ForeignKey('fast_links.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    fast_links_id = db.Column(db.Integer, db.ForeignKey('fast_links.id'), nullable=False)
     country_code = db.Column(db.String(2), nullable=False)
     visit_date = db.Column(db.DateTime, nullable=False)
 
-    fast_links = db.relationship('FastLinks', backref=db.backref('visitors', cascade='all, delete-orphan'))
+    fast_links = db.relationship('FastLinks', backref=db.backref('visitors', cascade='all'))
 
     @property
     def serialize(self):
         """Return object data in easily serializable format"""
         """https://stackoverflow.com/a/7103486"""
-        # TODO: Add serialize property for created_at
-        # and expire
         return {
             "id": self.id,
             "fast_links_id": self.fast_links_id,
